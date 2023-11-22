4d784fe78a6a4f12d564a37f4d39e3766503ddac
AlexisRelinCarbon-it <alexis.relin@carbon-it.com>
2023-11-22 14:31:57 +0100

    base project

---
+++ bank/src/main/java/com/carbon/bank/BankApplication.java
@@ -0,0 +1,8 @@
+package com.carbon.bank;
+
+public class BankApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ bank/src/test/java/com/carbon/bank/BankApplicationTests.java
@@ -0,0 +1,11 @@
+package com.carbon.bank;
+
+import org.junit.jupiter.api.Test;
+
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
